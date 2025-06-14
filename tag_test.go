04a267f77d58441ae702ec1da3cb70a30e6b0c29
xbl4de <216059651+xbl4de@users.noreply.github.com>
2025-06-14 20:20:50 +0300

    Add tests

---
+++ tag_test.go
@@ -0,0 +1,66 @@
+package yadi
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestParseTag_EmptyTag(t *testing.T) {
+	tag, err := ParseTag("")
+	require.NoError(t, err)
+	require.Equal(t, "", tag.BeanName)
+	require.Equal(t, false, tag.Ignore)
+	require.Equal(t, "", tag.ValuePath)
+}
+
+func TestParseTag_FullTag(t *testing.T) {
+	tag, err := ParseTag("ignore;path=abc;beanName=def")
+	require.NoError(t, err)
+	require.Equal(t, "abc", tag.ValuePath)
+	require.Equal(t, "def", tag.BeanName)
+	require.Equal(t, true, tag.Ignore)
+}
+
+func TestParseTag_IgnoreTag(t *testing.T) {
+	tag, err := ParseTag("ignore")
+	require.NoError(t, err)
+	require.Equal(t, "", tag.BeanName)
+	require.Equal(t, "", tag.ValuePath)
+	require.Equal(t, true, tag.Ignore)
+}
+
+func TestParseTag_BeanName(t *testing.T) {
+	tag, err := ParseTag("beanName=beanA")
+	require.NoError(t, err)
+	require.Equal(t, "beanA", tag.BeanName)
+	require.Equal(t, false, tag.Ignore)
+	require.Equal(t, "", tag.ValuePath)
+}
+
+func TestParseTag_ValuePath(t *testing.T) {
+	tag, err := ParseTag("path=abc")
+	require.NoError(t, err)
+	require.Equal(t, "abc", tag.ValuePath)
+	require.Equal(t, false, tag.Ignore)
+	require.Equal(t, "", tag.BeanName)
+}
+
+func TestParseTag_EmptyValuePath(t *testing.T) {
+	_, err := ParseTag("path=")
+	require.ErrorIs(t, err, ErrParseTag)
+}
+
+func TestParseTag_EmptyBeanName(t *testing.T) {
+	_, err := ParseTag("beanName=")
+	require.ErrorIs(t, err, ErrParseTag)
+}
+
+func TestParseTag_UnknownTag(t *testing.T) {
+	_, err := ParseTag("unknown")
+	require.ErrorIs(t, err, ErrParseTag)
+}
+
+func TestParseTag_ExtraValues(t *testing.T) {
+	_, err := ParseTag("beanName=beanA=beanB")
+	require.ErrorIs(t, err, ErrParseTag)
+}
