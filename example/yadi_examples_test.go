6666d49c0bbcb5db3a6d9fa7dfe0bb15719106f3
xbl4de <216059651+xbl4de@users.noreply.github.com>
2025-06-29 10:49:21 +0300

    Add examples test

---
+++ example/yadi_examples_test.go
@@ -0,0 +1,78 @@
+package example
+
+import (
+	"fmt"
+	g "github.com/onsi/gomega"
+	"github.com/xbl4de/yadi"
+	"github.com/xbl4de/yadi/types"
+	"testing"
+)
+
+type ExampleServiceA struct {
+	Timeout     int    `yadi:"path=serviceA.timeout"`
+	Credentials string `yadi:"path=serviceA.credentials"`
+}
+
+type ExampleServiceB struct {
+	Timeout  int `yadi:"path=serviceB.timeout"`
+	ServiceA *ExampleServiceA
+}
+
+func TestAutoBeanProvideExample(t *testing.T) {
+	g.RegisterTestingT(t)
+
+	yadi.UseLazyContext()
+	yadi.SetValue("serviceA.timeout", 10)
+	yadi.SetValue("serviceB.timeout", 20)
+	yadi.SetValue("serviceA.credentials", `{"user":"password"}`)
+
+	b, err := yadi.GetBean[*ExampleServiceB]()
+	g.Expect(err).ShouldNot(g.HaveOccurred())
+
+	fmt.Printf("Service B: %+v\n", b)
+	fmt.Printf("Service A: %+v\n", b.ServiceA)
+}
+
+func TestBeanProviderExample(t *testing.T) {
+	g.RegisterTestingT(t)
+	yadi.UseLazyContext()
+
+	yadi.SetBeanProvider[*ExampleServiceB](func(ctx types.Context) (*ExampleServiceB, error) {
+		return &ExampleServiceB{
+			ServiceA: &ExampleServiceA{
+				Timeout:     15,
+				Credentials: `{"user1":"password1"}`,
+			},
+			Timeout: 25,
+		}, nil
+	})
+
+	b, err := yadi.GetBean[*ExampleServiceB]()
+	g.Expect(err).ShouldNot(g.HaveOccurred())
+
+	fmt.Printf("Service B: %+v\n", b)
+	fmt.Printf("Service A: %+v\n", b.ServiceA)
+}
+
+func TestFuncBeanProviderExample(t *testing.T) {
+	g.RegisterTestingT(t)
+
+	yadi.UseLazyContext()
+
+	provider := func() *ExampleServiceB {
+		return &ExampleServiceB{
+			ServiceA: &ExampleServiceA{
+				Timeout:     5,
+				Credentials: `{"user2":"password2"}`,
+			},
+			Timeout: 35,
+		}
+	}
+
+	yadi.SetBeanProviderFunc[*ExampleServiceB](provider)
+
+	b, err := yadi.GetBean[*ExampleServiceB]()
+	g.Expect(err).ShouldNot(g.HaveOccurred())
+	fmt.Printf("Service B: %+v\n", b)
+	fmt.Printf("Service A: %+v\n", b.ServiceA)
+}
--- go.mod
+++ go.mod
@@ -5,14 +5,11 @@ go 1.24.2
 require (
 	github.com/onsi/gomega v1.37.0
 	github.com/pkg/errors v0.9.1
-	github.com/stretchr/testify v1.10.0
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/net v0.37.0 // indirect
-	golang.org/x/text v0.23.0 // indirect
+	golang.org/x/net v0.41.0 // indirect
+	golang.org/x/text v0.26.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
